3632a904025437784cd131fdc0b5d8e576cd9d08
Catherine Lin <mscatherinelin@gmail.com>
2017-12-19 22:40:49 -0800

    Initial Commit

---
+++ TrueFalseStarter/TriviaGenerator.swift
@@ -0,0 +1,18 @@
+//
+//  triviaGenerator.swift
+//  TrueFalseStarter
+//
+//  Created by Catherine Lin on 12/19/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+struct TriviaGenerator {
+    let trivia: [[String : String]] = [
+        ["Question": "Only female koalas can whistle", "Answer": "False"],
+        ["Question": "Blue whales are technically whales", "Answer": "True"],
+        ["Question": "Camels are cannibalistic", "Answer": "False"],
+        ["Question": "All ducks are birds", "Answer": "True"]
+    ]
+    
+}
+
